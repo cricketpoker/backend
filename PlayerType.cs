3fdede9340dd7b455063941961701d2b004b0fe1
LetsDoItNow <sohammmehta@gmail.com>
2015-03-18 22:25:40 -0700

    adding .cs files for question generation

---
+++ PlayerType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketQuestionGeneration
+{
+    [Flags]
+    internal enum PlayerType
+    {
+        Batsman = 0x1,
+        Bowler = 0x2,
+        WicketKeeper = 0x4,
+        AllRounder = Batsman | Bowler,
+        WicketKeeperBatsman = WicketKeeper | Batsman,
+    }
+}
